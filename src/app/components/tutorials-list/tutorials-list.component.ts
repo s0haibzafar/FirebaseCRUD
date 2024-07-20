e1a2d7325dda622ed77609e1c1abdb3c9300e294
SohaibZafar1986 <sohaib@orenda.systems>
2024-07-20 19:54:09 +0500

    Create tutorial list component

---
+++ src/app/components/tutorials-list/tutorials-list.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Tutorial } from 'src/app/models/tutorial.model';
+import { TutorialService } from 'src/app/services/tutorial.service';
+
+@Component({
+  selector: 'app-tutorials-list',
+  templateUrl: './tutorials-list.component.html',
+  styleUrls: ['./tutorials-list.component.css']
+})
+export class TutorialsListComponent implements OnInit {
+
+  tutorials?: Tutorial[];
+  currentTutorial?: Tutorial;
+  currentIndex = -1;
+  title = '';
+
+  constructor(private tutorialService: TutorialService) { }
+
+  ngOnInit(): void {
+    this.retrieveTutorials();
+  }
+
+  refreshList(): void {
+    this.currentTutorial = undefined;
+    this.currentIndex = -1;
+    this.retrieveTutorials();
+  }
+
+  retrieveTutorials(): void {
+    this.tutorialService.getAll().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+        {
+          return  { key: c.payload.key, ...c.payload.val() }
+        }
+        )
+      )
+    ).subscribe(data => {
+      this.tutorials = data;
+      console.log("LISt->>>", this.tutorials);
+    });
+  }
+
+  setActiveTutorial(tutorial: Tutorial, index: number): void {
+    this.currentTutorial = tutorial;
+    this.currentIndex = index;
+  }
+
+  removeAllTutorials(): void {
+    this.tutorialService.deleteAll()
+      .then(() => this.refreshList())
+      .catch(err => console.log(err));
+  }
+
+}
